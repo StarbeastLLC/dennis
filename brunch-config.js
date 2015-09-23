6a9b34a4ec777e900f40ee4c672441b5aca76e57
Zura Guerra <zuraweep@gmail.com>
2015-09-22 23:06:53 -0500

    Add jquery and config brunch to produce vendor.js from bower_components

--- brunch-config.js
+++ brunch-config.js
@@ -2,23 +2,22 @@ exports.config = {
   // See http://brunch.io/#documentation for docs.
   files: {
     javascripts: {
-      joinTo: 'js/app.js'
+      //joinTo: 'js/app.js'
 
       // To use a separate vendor.js bundle, specify two files path
       // https://github.com/brunch/brunch/blob/stable/docs/config.md#files
-      // joinTo: {
-      //  'js/app.js': /^(web\/static\/js)/,
-      //  'js/vendor.js': /^(web\/static\/vendor)|(deps)/
-      // }
+      joinTo: {
+        'js/app.js': /^(web\/static\/js)/,
+        'js/vendor.js': /^(web\/static\/vendor)|(deps)|(bower_components)/
+      },
       //
       // To change the order of concatenation of files, explicitly mention here
       // https://github.com/brunch/brunch/tree/master/docs#concatenation
-      // order: {
-      //   before: [
-      //     'web/static/vendor/js/jquery-2.1.1.js',
-      //     'web/static/vendor/js/bootstrap.min.js'
-      //   ]
-      // }
+      order: {
+         before: [
+           'bower_components/jquery/dist/jquery.js',
+         ]
+      }
     },
     stylesheets: {
       joinTo: 'css/app.css'
