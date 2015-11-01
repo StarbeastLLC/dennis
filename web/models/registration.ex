3bc61a11d710b8d2f33b7acf6a4037583d35a2b5
Zura Guerra <zuraweep@gmail.com>
2015-11-01 17:57:47 -0600

    Create a org cause on registration

--- web/models/registration.ex
+++ web/models/registration.ex
@@ -1,6 +1,8 @@
 defmodule Dennis.Registration do
   import Ecto.Changeset, only: [put_change: 3]
 
+  alias Dennis.Cause
+
   def create(changeset) do
     changeset
     |> put_change(:hashed_pswd, hashed_password(changeset.params["password"]))
@@ -11,6 +13,24 @@ defmodule Dennis.Registration do
     changeset
     |> put_change(:hashed_pswd, hashed_password(changeset.params["password"]))
     |> Dennis.Repo.update
+    |> create_org_as_cause
+  end
+
+  defp create_org_as_cause({:error, invalid_user_changeset} = error) do
+     error
+  end
+
+  defp create_org_as_cause({:ok, user}) do
+    changeset = Cause.changeset %Cause{}, %{
+      name: user.org_name,
+      country: user.country,
+      state: user.state,
+      description: user.description,
+      more_info: user.website,
+      user_id: user.id
+    }
+    Dennis.Repo.insert!(changeset)
+    {:ok, user}
   end
 
   defp hashed_password(password) do
