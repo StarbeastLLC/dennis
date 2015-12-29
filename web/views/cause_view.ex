b800d0164ae156d98a56c7b9f5c46d7c8e4bd0f0
Zura Guerra <zuraweep@gmail.com>
2015-12-29 02:34:59 -0600

    Fix cause and charity show support images lightbox

--- web/views/cause_view.ex
+++ web/views/cause_view.ex
@@ -12,4 +12,25 @@ defmodule Dennis.CauseView do
   def photo_url(cause) do
     Dennis.CausePhoto.url({cause.photo1, cause})
   end
+
+  def cause_support_photos(cause) do
+    cause
+    |> Map.take([:photo2, :photo3, :photo4])
+    |> Map.values
+    |> Enum.filter(fn p -> p end)
+    |> Enum.map(fn p -> Dennis.CausePhoto.url({p, cause}) end)
+  end
+
+  def parse_video_url(watch_url) do
+    [video_id] = String.split watch_url, "https://www.youtube.com/watch?v=", trim: true
+    "https://youtube.com/embed/" <> video_id
+  end
+
+  def cause_main_photo(cause) do
+    if cause.photo1 do
+      Dennis.CausePhoto.url({cause.photo1, cause})
+    else
+      "/images/c1.jpg"
+    end
+  end
 end
