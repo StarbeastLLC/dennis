59093faa921e63016c88494e2f5b39d1d0afb8b8
Zura Guerra <zuraweep@gmail.com>
2015-12-29 03:58:45 -0600

    Add form validations with HTML

--- web/views/cause_view.ex
+++ web/views/cause_view.ex
@@ -21,6 +21,14 @@ defmodule Dennis.CauseView do
     |> Enum.map(fn p -> Dennis.CausePhoto.url({p, cause}) end)
   end
 
+  def charity_support_photos(cause) do
+    cause
+    |> Map.take([:photo1, :photo2, :photo3, :photo4])
+    |> Map.values
+    |> Enum.filter(fn p -> p end)
+    |> Enum.map(fn p -> Dennis.CausePhoto.url({p, cause}) end)
+  end
+
   def parse_video_url(watch_url) do
     [video_id] = String.split watch_url, "https://www.youtube.com/watch?v=", trim: true
     "https://youtube.com/embed/" <> video_id
